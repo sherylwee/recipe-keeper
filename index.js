cf13ae98a76422c915f0f2476d26d404b36518c1
root <root@LAPTOP-V1DATB7R.localdomain>
2019-01-07 13:51:08 +0800

    consoleLogged

--- index.js
+++ index.js
@@ -81,6 +81,7 @@ app.get('/recipes/:id/edit', (request, response) => {
         jsonfile.readFile(file, (err, obj) => {
             let searchedRecipe = obj.recipes[id];
             console.log("testing ");
+            console.log("Well done");
             response.render("edit", searchedRecipe)
 
         });
